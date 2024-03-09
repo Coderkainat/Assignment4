419acaa5a57a293020b38d9f07dda8ce951fe221
Coderkainat <159674299+Coderkainat@users.noreply.github.com>
2024-03-09 18:34:12 +0500

    Add files via upload

---
+++ class4 assignment/main.js
@@ -0,0 +1,16 @@
+var percentage = "90";
+if (percentage >= "90") {
+    console.log("A+ Grade");
+}
+else if (percentage >= "80") {
+    console.log("grade: A ");
+}
+else if (percentage >= "70") {
+    console.log("B Grade");
+}
+else if (percentage >= "60") {
+    console.log("C grade");
+}
+else {
+    console.log("fail");
+}
